e1326fec5f61f69682a4291b2d39f3dcc8872ef1
Arash <sainiarash555@gmail.com>
2024-10-20 15:30:32 +0530

    project

---
+++ array.js
@@ -0,0 +1,27 @@
+let a=["d1.jpg","d2.jpeg","d3.jpeg","d4.jpeg","d5.png","d6.png"];
+    function arash(){
+        let n1=parseInt(Math.random()*6);
+        h3.innerHTML=[n1+1];
+        document.images[1].src=a[n1];
+        show();
+    }
+    function aman(){
+        let n=parseInt(Math.random()*6);
+        h.innerHTML=[n+1];
+        document.images[0].src=a[n];
+        show();
+    }
+        function show(){
+        let score1=h.innerHTML;
+        let score2=h3.innerHTML;
+        let result;
+    if(score1>score2){
+    result="player1 is winner";
+    }
+    else if(score1<score2){
+    result="player2 is winner";
+    } else{
+    result="game is drawn" ;
+    }
+    document.getElementById("h1").innerHTML=result;
+        }   
