861c7eecf7954dc364afaf24672c369daa08bb57
Arash <sainiarash555@gmail.com>
2024-10-23 14:58:01 +0530

    array

--- array7.js
+++ array7.js
@@ -1,24 +1,39 @@
 let a =["d1.jpg","d2.jpeg","d3.jpeg","d4.jpeg","d5.png","d6.png"];
-    function arash(){
-        let n1=parseInt(Math.random()*6);
-        h3.innerHTML=[n1+1];
-        document.images[1].src=a[n1];
-        show();
-    }
+let b=[];
+let c=[];
+let sum=0;
+let sum1=0;
     function aman(){
         let n=parseInt(Math.random()*6);
-        h.innerHTML=[n+1];
         document.images[0].src=a[n];
+        b.push(n+1);
+        console.log(n+1)
+        for( let i=0;i<b.length;i++){
+            sum=sum+b[i]
+            h.innerHTML=sum;
+                 }
+                 console.log(sum)
+        show();
+    }
+    function arash(){
+        let n=parseInt(Math.random()*6);
+        document.images[1].src=a[n];
+        c.push(n+1);
+        for( let i=0;i<c.length;i++){
+            sum1=sum1+c[i]
+            h3.innerHTML=sum1;
+                 }
+                 console.log(sum)
         show();
     }
         function show(){
-        let score1=h.innerHTML;
-        let score2=h3.innerHTML;
+        let b=h3.innerHTML;
+        let c=h.innerHTML;
         let result;
-    if(score1>score2){
+    if(sum>sum1){
     result="player1 is winner";
     }
-    else if(score1<score2){
+    else if(sum<sum1){
     result="player2 is winner";
     } else{
     result="game is drawn" ;
